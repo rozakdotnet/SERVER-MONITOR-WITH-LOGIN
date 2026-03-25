6f1cbd8e9f98915004ee8146cbe9534ea8fd1c23
rozakdotnet <68309084+rozakdotnet@users.noreply.github.com>
2026-03-25 09:47:55 +0800

    Create about.php

---
+++ about.php
@@ -0,0 +1,31 @@
+<?php require "part/head.php";?><section>
+<h2>ABOUT</h2>
+<div class="card">
+<p>Ini adalah homemeserver kedua saya, ianya bermula ketika saya memerlukan sistem muat turun harian data cadangan dari beberapa laman web yang saya uruskan.</p>
+<img alt="HOMESERVER" title="Internet route" src="img/dbsvr.png" class="imgabt"/>
+<h3>AdGuard Home</h3>
+<p>AdGuard adalah lapisan pertama yang menghubungkan rangkaian di rumah ke internet. Disamping sebagai pelayan dns fungsi utamanya ialah untuk menyekat iklan. Selain daripada itu AdGuard berfungsi sebagai sistem kawalan ibu bapa dalam menyekat laman web yang tidak seuai untuk seisi rumah.</p><p>Dns sendiri adalah sistem yang menterjemahkan nama domain kepada alamat sebenar lokasi laman web iaitu nomobor IP ketika dilayari. Setiap pembekal internet mempunyai pelayan dns, dari sanalah kerajaan membuat sekatan keatas laman web tertentu yang dianggap menyalahi undang-undang.</p>
+<img alt="HOMESERVER" title="Internet route" src="img/debi.jpg" class="imgabt"/>
+<h3>Ciri-ciri Pelayan:</h3>
+<ul><li>Cpu i5 4460T 4 teras</li>
+<li>Papan induk B85 MSI</li>
+<li>Ram 12GB DDR3</li>
+<li>Ssd Ediloca 128GB (OS)</li>
+<li>Sshd Seagate 1TB</li>
+<li>Hdd WD Blue 500GB</li>
+<li>Gpu GT610 DDR3 1GB</li>
+<li>Psu Armageddon 250w</li>
+<li>Pc case ICE Whale ST01</li>
+</ul>
+<p>Kecuali pc case, psu dan kipas rgb kesemua perkakasan tersebut adalah barang terpakai. Cpu, sshd Seagate dan ram 8GB berasal dari komputer lama saya yang rosak, ram 4 gb dari mini homeserver sebelum ini (HP thin client 620). Ssd ediloca dari komputer harian. Manakal gpu, WD Blue, B85 barang terpakai dari shopee. Disebabkan papan induk tidak mempunyai sokongan hdmi gpu diperlukan hanya apabila perlu memasang semula sistem pengeporasian.</p>
+<h4>Perisian:</h4>
+<ul><li>OS: Linux</li>
+<li>Distro: Debian</li>
+<li>AdGuard Home + unbound</li>
+<li>Cloudflare</li>
+<li>File Browser</li>
+<li>Immich</li>
+<li>OpenliteSpeed + lsphp</li>
+</ul>
+</div>
+</section><style>.imgabt {margin:5px auto;max-width: 100%;height: auto;display: block;}.dark .imgabt{filter: brightness(50%);}</style><?php require "part/foot.php";?>
