b8bacc76c4851329696b80386b94d255f35d7842
rozakdotnet <68309084+rozakdotnet@users.noreply.github.com>
2026-03-25 08:29:48 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+$config = require 'config.php';
+
+if (isset($_SESSION['login'])) {
+  header('Location: index.php');
+  exit;
+}
+
+$error = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if ($_POST['username'] === $config['username'] &&
+      password_verify($_POST['password'], $config['password'])) {
+
+    $_SESSION['login'] = true;
+    $_SESSION['last'] = time();
+
+    header('Location: index.php');
+    exit;
+  } else {
+    $error = "Login salah";
+  }
+}
+?>
+
+<form method="POST">
+  <h2>Server Login</h2>
+  <?php if($error) echo "<p>$error</p>"; ?>
+  <input name="username" placeholder="Username" required>
+  <input name="password" type="password" placeholder="Password" required>
+  <button>Login</button>
+</form>
