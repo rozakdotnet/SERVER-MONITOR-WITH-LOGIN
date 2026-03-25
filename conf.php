8f569a96e8c0cd4ad80c41a6149a3fd5f22c5847
rozakdotnet <68309084+rozakdotnet@users.noreply.github.com>
2026-03-25 08:32:49 +0800

    Create conf.php

---
+++ conf.php
@@ -0,0 +1,17 @@
+<?php
+return [
+
+  // 🔐 login
+  'auth' => [
+    'username' => 'admin',
+    'password' => '$2y$10$xxxxx'
+  ],
+
+  // 📋 title/menu
+  'title' => [
+    'index' => 'Dashboard',
+    'server' => 'Server Monitor',
+    'dns' => 'DNS Log'
+  ]
+
+];
