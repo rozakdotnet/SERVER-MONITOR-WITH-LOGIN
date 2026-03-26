e2e4a0e9f7585b6cdc358a1e5ab562a6e85b6c17
rozakdotnet <68309084+rozakdotnet@users.noreply.github.com>
2026-03-26 08:01:13 +0800

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,4 +1,4 @@
 <?php
 session_start();
 if (!isset($_SESSION['bossku'])) { header('Location: /login.php'); exit;}if (time() - $_SESSION['bossku'] > 1800) { session_destroy(); header('Location: /login.php'); exit;}
-$_SESSION['last'] = time();
+$_SESSION['bossku'] = time();
